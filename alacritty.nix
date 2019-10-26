67790cd52e2dab352550a4d81dee05bc79a6a53f
Alan Hollis <me@alanhollis.com>
2019-10-26 17:33:15 +0100

    Initial commit

---
+++ alacritty.nix
@@ -0,0 +1,105 @@
+{ zsh }:''
+env:
+  TERM: screen-256color
+
+window:
+  dimensions:
+    columns: 0
+    lines: 0
+
+  padding:
+    x: 2
+    y: 2
+
+  decorations: full
+
+tabspaces: 4
+
+draw_bold_text_with_bright_colors: true
+
+font:
+  normal:
+    family: "Fira Code"
+    style: Retina
+
+  # The bold font face
+  bold:
+    family: "Fira Code"
+    style: Bold
+
+  # The italic font face
+  italic:
+    family: "Fira Code"
+  size: 8.0
+
+debug.render_timer: false
+
+# Base16 Default Dark 256 - alacritty color config
+# Chris Kempson (http://chriskempson.com)
+colors:
+  # Default colors
+  primary:
+    background: '0x181818'
+    foreground: '0xd8d8d8'
+
+  # Colors the cursor will use if `custom_cursor_colors` is true
+  cursor:
+    text: '0x181818'
+    cursor: '0xd8d8d8'
+    style: Block
+    unfocused_hollow: true
+
+  # Normal colors
+  normal:
+    black:   '0x181818'
+    red:     '0xab4642'
+    green:   '0xa1b56c'
+    yellow:  '0xf7ca88'
+    blue:    '0x7cafc2'
+    magenta: '0xba8baf'
+    cyan:    '0x86c1b9'
+    white:   '0xd8d8d8'
+
+  # Bright colors
+  bright:
+    black:   '0x585858'
+    red:     '0xab4642'
+    green:   '0xa1b56c'
+    yellow:  '0xf7ca88'
+    blue:    '0x7cafc2'
+    magenta: '0xba8baf'
+    cyan:    '0x86c1b9'
+    white:   '0xd8d8d8'
+
+visual_bell:
+  animation: EaseOutExpo
+  duration: 0
+
+# Background opacity
+background_opacity: 1.0
+
+mouse_bindings:
+  - { mouse: Middle, action: PasteSelection }
+
+mouse:
+  double_click: { threshold: 300 }
+  triple_click: { threshold: 300 }
+
+  faux_scrolling_lines: 1
+
+  hide_when_typing: true
+
+selection:
+  semantic_escape_chars: ",?`|:\"' ()[]{}<>"
+  save_to_clipboard: true
+
+dynamic_title: true
+
+
+live_config_reload: true
+
+shell:
+  program: ${zsh}/bin/zsh
+  args:
+    - --login
+''
---
+++ configuration.nix
@@ -0,0 +1,183 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+    pureZshPrompt = pkgs.fetchgit {
+      url = "https://github.com/sindresorhus/pure";
+      rev = "e7036c43487fedf608a988dde54dd1d4c0d96823";
+      sha256 = "10mdk4dn2azzrhymx0ghl8v668ydy6mz5i797nmbl2ijx9hlqb3v";
+    };
+
+
+in {
+
+  
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ 
+   "pci=noaer i915.enable_guc=3 acpi=off" 
+ ];
+
+
+  system.userActivationScripts = {
+   extraUserActivation = {
+       text = ''
+        ln -sfn /etc/per-user/alacritty ~/.config/
+        mkdir -p ~/.zfunctions
+        ln -sfn ${pureZshPrompt}/pure.zsh ~/.zfunctions/prompt_pure_setup
+        ln -sfn ${pureZshPrompt}/async.zsh ~/.zfunctions/async
+      '';
+      deps = [];
+    };
+  };
+
+
+  boot.kernelModules = ["kvm-intel"];
+  
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
+
+  networking.hostName = "PLUSNET-QXGK"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+     wget vim google-chrome fwupd efivar systool gns3-gui gns3-server vscode
+     zip p7zip git qemu gnumake gcc wireshark libpcap tigervnc telnet htop
+     alacritty
+   ];
+
+   environment.etc = {
+    "per-user/alacritty/alacritty.yml".text = import ./alacritty.nix { zsh = pkgs.zsh; };
+  };
+  environment.sessionVariables.TERMINAL = [ "alacritty" ];
+
+  
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting = {
+      enable = true;
+    };
+    promptInit = ''
+      fpath=( "$HOME/.zfunctions" $fpath )
+      autoload -U promptinit && promptinit && prompt pure
+    '';
+    interactiveShellInit = ''
+      autoload -U up-line-or-beginning-search
+      bindkey '^[[A' up-line-or-beginning-search
+      zle -N up-line-or-beginning-search
+    '';
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts
+    dina-font
+    proggyfonts
+  ];
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "gb";
+    videoDrivers = [ "intel" ]; 
+    displayManager.slim.enable = true;
+  };
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager = {
+    default = "none";
+    xterm.enable = false;
+  };
+  services.xserver.windowManager.i3.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.alan = {
+     isNormalUser = true;
+     uid = 1000;
+     home = "/home/alan";
+     extraGroups = [ "wheel" "networkmanager" ];
+     shell = pkgs.zsh;
+   };
+
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+  systemd.packages = [ pkgs.fwupd ];
+  services.sshd.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+
+
+
+
+}
