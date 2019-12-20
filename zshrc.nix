5f769eeea1782bfb581852987b021b73ab330767
Alan Hollis <me@alanhollis.com>
2019-12-20 17:12:21 +0000

    Force zshrc to save history on every keypress

--- zshrc.nix
+++ zshrc.nix
@@ -1,6 +1,8 @@
 { zsh }:''
 bindkey -v
 bindkey '^r' history-incremental-search-backward
+setopt inc_append_history
+setopt share_history
 if [ "$TMUX" = "" ]; then tmux; fi
 xrandr --output HDMI1 --auto --output HDMI2 --auto --right-of HDMI1
 ''
