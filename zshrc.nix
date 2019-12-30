42144d2b33503f6e766e2cb72e44b6241b34bfd7
Alan Hollis <me@alanhollis.com>
2019-12-30 20:01:20 +0000

    Fix xrandr with new hardware

--- zshrc.nix
+++ zshrc.nix
@@ -4,5 +4,5 @@ bindkey '^r' history-incremental-search-backward
 setopt inc_append_history
 setopt share_history
 if [ "$TMUX" = "" ]; then tmux; fi
-xrandr --output HDMI1 --auto --output HDMI2 --auto --right-of HDMI1
+xrandr --output HDMI-2 --auto --output DP-1 --auto --right-of HDMI-2
 ''
