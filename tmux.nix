51f6bc48006359809b41032d7ba1f3735c618a13
Alan Hollis <me@alanhollis.com>
2019-12-31 10:13:53 +0000

    Fix mouse copy and pasting

--- tmux.nix
+++ tmux.nix
@@ -6,7 +6,7 @@ set -g status-keys vi
 set -g mode-keys vi
 bind P paste-buffer
 bind-key    -T copy-mode    C-w               send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
-bind-key    -T copy-mode    MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
+bind-key    -T copy-mode    MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "tmux-save-buffer - | xclip -selection primary -in -f | xclip -selection clipboard -in"
 bind-key    -T copy-mode    M-w               send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
 bind-key    -T copy-mode-vi Enter             send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
 bind-key    -T copy-mode-vi v                 send-keys -X begin-selection
