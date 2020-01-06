338d28a68192de902d1dff9d6c8db59efb0ee3b6
Alan Hollis <me@alanhollis.com>
2020-01-06 18:18:17 +0000

    Use cloudflare name servers

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,7 @@ in {
 
   networking.hostName = "alan-nixos"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.nameservers = [ "1.0.0.1" "1.1.1.1" ];
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
