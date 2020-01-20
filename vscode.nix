9432868c66c27dfd8843482689d3ac5721cdca74
Alan Hollis <me@alanhollis.com>
2020-01-20 15:02:33 +0000

    Add more extensions for vscode

--- vscode.nix
+++ vscode.nix
@@ -33,7 +33,32 @@ let
           version = "0.9.0";
           sha256 = "0wka4sgq5xjgqq2dc3zimrdcbl9166lavscz7zm6v4n6f9s2pfj0";
 	}
-      ];
+ 	{
+	  name = "dotnet-test-explorer";
+	  publisher = "formulahendry";
+          version = "0.7.1";
+          sha256 = "1v80dclx4l8iza27d0d5ybfywdz3rid07h05rs0hwpiyfpb23pw8";
+	}
+	{
+	  name = "vscode-coverage-gutters";
+	  publisher = "ryanluker";
+          version = "2.4.2";
+          sha256 = "1351ix5vp1asny6c2dihf8n18gdqsg5862z6wx2b5yflvslsqjx2";
+	}
+	{
+	  name = "terraform";
+	  publisher = "mauve";
+          version = "1.4.0";
+          sha256 = "0b3cqxaay85ab10x1cg7622rryf4di4d35zq9nqcjg584k6jjb34";
+	}
+	{
+	  name = "code-spell-checker";
+	  publisher = "streetsidesoftware";
+          version = "1.4.0";
+          sha256 = "0cjhglyqrwvi0b1pw20idi1z1q6fq3yv98kvr433d76p9bzz3fkj";
+	}
+
+     ];
   };
 in 
   vscode
