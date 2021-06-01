3f168a14f76133d4a677de60c34ae73a33e52e93
Alan Hollis <me@alanhollis.com>
2021-06-01 10:03:16 +0100

    Change to use DP-2

--- zshrc.nix
+++ zshrc.nix
@@ -16,5 +16,5 @@ setopt EXTENDED_HISTORY
 alias history="fc -l -50" 
 if [ "$TMUX" = "" ]; then tmux; fi
 . ~/bin/z.sh
-xrandr --output HDMI-2 --auto --output DP-1 --auto --right-of HDMI-2
+xrandr --output DP-2 --auto --output DP-1 --auto --right-of DP-2
 ''
