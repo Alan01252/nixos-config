5358e5133e9829afa3cb2df49ea0bde51eae132c
Alan Hollis <me@alanhollis.com>
2021-11-13 13:25:03 +0000

    Update flux

--- flux.nix
+++ flux.nix
@@ -3,8 +3,8 @@
 pkgs.stdenv.mkDerivation {
   name = "flux2";
   src = pkgs.fetchzip {
-    url = "https://github.com/fluxcd/flux2/releases/download/v0.16.1/flux_0.16.1_linux_amd64.tar.gz";
-    sha256 = "09pj97sklj4ryv2jppba1gj142rv1qlmy36bd705ym0wb7n3ym9b";
+    url = "https://github.com/fluxcd/flux2/releases/download/v0.21.0/flux_0.21.0_linux_amd64.tar.gz";
+    sha256 = "0087ffgxmn0i8g74kchra9sr25js1nw8cjpxsfzq62h1wr34y0qx";
   };
   phases = ["installPhase" "patchPhase"];
   installPhase = ''
