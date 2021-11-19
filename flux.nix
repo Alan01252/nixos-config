aaf1a6f8ab1fb932fa3148afb8f69b713d868616
Alan Hollis <me@alanhollis.com>
2021-11-19 21:24:14 +0000

    Bump to latest flux

--- flux.nix
+++ flux.nix
@@ -3,8 +3,8 @@
 pkgs.stdenv.mkDerivation {
   name = "flux2";
   src = pkgs.fetchzip {
-    url = "https://github.com/fluxcd/flux2/releases/download/v0.21.0/flux_0.21.0_linux_amd64.tar.gz";
-    sha256 = "0087ffgxmn0i8g74kchra9sr25js1nw8cjpxsfzq62h1wr34y0qx";
+    url = "https://github.com/fluxcd/flux2/releases/download/v0.23.0/flux_0.23.0_linux_amd64.tar.gz";
+    sha256 = "eax9N/bxbm0k25d1RCfgRnQUbE4Wi+QyajlFG8xd5GE=";
   };
   phases = ["installPhase" "patchPhase"];
   installPhase = ''
