0830bbf8b534abcf99153f7da6cb016252ebbff5
Alan Hollis <me@alanhollis.com>
2022-07-26 08:22:29 +0100

    Update flux version

--- flux.nix
+++ flux.nix
@@ -3,8 +3,8 @@
 pkgs.stdenv.mkDerivation {
   name = "flux2";
   src = pkgs.fetchzip {
-    url = "https://github.com/fluxcd/flux2/releases/download/v0.23.0/flux_0.23.0_linux_amd64.tar.gz";
-    sha256 = "eax9N/bxbm0k25d1RCfgRnQUbE4Wi+QyajlFG8xd5GE=";
+    url = "https://github.com/fluxcd/flux2/releases/download/v0.28.5/flux_0.28.5_linux_amd64.tar.gz";
+    sha256 = "sha256-IIK5yNj3kbpKhrNF65ESiYgadgAcsvK5BFXxbwkdvHg=";
   };
   phases = ["installPhase" "patchPhase"];
   installPhase = ''
